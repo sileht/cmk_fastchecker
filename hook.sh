26ffba1dd1372f0a29a668eadc6e641187e442f4
Mehdi Abaakouk <sileht@sileht.net>
2018-02-13 14:23:12 +0100

    Remove usage of subshell

--- hook.sh
+++ hook.sh
@@ -8,21 +8,23 @@ min() {
 }
 
 SITENAME="$(id -un)"
+TMPDIR="/omd/sites/$SITENAME/tmp/fastchecker/hooks"
 CMK_FASTPINGER_DUMP="/omd/sites/$SITENAME/tmp/fastpinger/fastpinger.dump"
+mkdir -p $TMPDIR
+
+NAMED_PIPE=$(mktemp --dry-run $TMPDIR/pipe.XXXXXX)
+mkfifo $NAMED_PIPE
+cleanup () { rm -f $NAMED_PIPE ; }
+trap cleanup EXIT
 
 mode=$1
 shift
 if [ "$mode" == "check" -o "$mode" == "inventory" ]; then
     host="$1"
-    curl -s http://localhost:5001/$mode/$host | (
-        read ret; cat ; exit $ret
-    )
-    ret=$?
-    if [ "$ret" -gt 3 ]; then
+    curl -s http://localhost:5001/$mode/$host > $NAMED_PIPE &
+    { read RET ; cat ;} < $NAMED_PIPE
+    if [[ -z "$RET" ]] || [[ "$RET" -gt 3 ]]; then
         echo "fastchecker unreachable"
-        exit 3
-    else
-        exit $ret
     fi
 elif [ "$mode" == "ping" ]; then
     fdate=$(stat  -c %x $CMK_FASTPINGER_DUMP | sed 's/\..*//g')
@@ -30,6 +32,8 @@ elif [ "$mode" == "ping" ]; then
     OPTS=`getopt -o 46w:c:n:i:I:m:l:t:b:H: -n 'parse-options' -- "$@"`
     eval set -- "$OPTS"
     ip_familly=4
+    warn="200,40"
+    crit="500,80"
     while true; do
         case "$1" in
             -4|-6) ip_familly=${1#-}; shift;;
@@ -55,46 +59,42 @@ elif [ "$mode" == "ping" ]; then
         loss_crit=$(echo $crit | awk -F, '{gsub(/\..*/, "", $2); print $2}')
 
         # Format: <ip> : 0.48 0.33 0.46 0.46 0.39
-        sed -n "s/^$dest\s\s*:\s*//gp" $CMK_FASTPINGER_DUMP | grep -v "duplicate" | (
-            read p1 p2 p3 p4 p5
-            if [ "$p1" -a "$p2" -a "$p3" -a "$p4" -a "$p5" ]; then
-                loss=0
-                count=0
-                math=""
-                [ "$p1" == "-" ] && loss=$((loss + 20)) || { math="$math + $p1"; count=$((count + 1)); }
-                [ "$p2" == "-" ] && loss=$((loss + 20)) || { math="$math + $p2"; count=$((count + 1)); }
-                [ "$p3" == "-" ] && loss=$((loss + 20)) || { math="$math + $p3"; count=$((count + 1)); }
-                [ "$p4" == "-" ] && loss=$((loss + 20)) || { math="$math + $p4"; count=$((count + 1)); }
-                [ "$p5" == "-" ] && loss=$((loss + 20)) || { math="$math + $p5"; count=$((count + 1)); }
-                
-                if [ $count -gt 0 ]; then
-                        rt=$(echo "(0 $math) / $count * 1000" | bc -l | awk '{gsub(/\..*/, "", $1); print $1}')
-                        rt_display=$(echo "scale=2; (0 $math) / $count" | bc -l)
-                        rt_min=$(min $p1 $p2 $p3 $p4 $p5)
-                        rt_max=$(max $p1 $p2 $p3 $p4 $p5)
-                else
-                        rt=0 rt_display=0 rt_min=0 rt_max=0
-                fi
-                state="OK"
-                [ $loss -ge $loss_warn -o $rt -ge $rt_warn ] && state="WARNING"
-                [ $loss -ge $loss_crit -o $rt -ge $rt_crit ] && state="CRITICAL"
-
-                echo "$state - $dest : rta ${rt_display}ms, lost ${loss}% (fastpinger at $fdate)|rta=${rt_display}ms;${rt_warn};${rt_cirt};0; pl=${loss}%;${loss_warn};${loss_crit};; rtmax=${rt_max}ms;;;; rtmin=${rt_min}ms;;;;"
-                case $state in
-                    OK) exit 0;;
-                    WARNING) exit 1;;
-                    CRITICAL) exit 2;;
-                    *) exit 3;;
-                esac
+        sed -n "s/^$dest\s\s*:\s*//gp" $CMK_FASTPINGER_DUMP | grep -v "duplicate" > $NAMED_PIPE &
+	{ read p1 p2 p3 p4 p5 ; cat > /dev/null ; } < $NAMED_PIPE  # cat is used to empty the pipe in case of duplicate
+        if [ "$p1" -a "$p2" -a "$p3" -a "$p4" -a "$p5" ]; then
+            loss=0
+            count=0
+            math=""
+            [ "$p1" == "-" ] && loss=$((loss + 20)) || { math="$math + $p1"; count=$((count + 1)); }
+            [ "$p2" == "-" ] && loss=$((loss + 20)) || { math="$math + $p2"; count=$((count + 1)); }
+            [ "$p3" == "-" ] && loss=$((loss + 20)) || { math="$math + $p3"; count=$((count + 1)); }
+            [ "$p4" == "-" ] && loss=$((loss + 20)) || { math="$math + $p4"; count=$((count + 1)); }
+            [ "$p5" == "-" ] && loss=$((loss + 20)) || { math="$math + $p5"; count=$((count + 1)); }
+            
+            if [ $count -gt 0 ]; then
+                    rt=$(echo "(0 $math) / $count * 1000" | bc -l | awk '{gsub(/\..*/, "", $1); print $1}')
+                    rt_display=$(echo "scale=2; (0 $math) / $count" | bc -l)
+                    rt_min=$(min $p1 $p2 $p3 $p4 $p5)
+                    rt_max=$(max $p1 $p2 $p3 $p4 $p5)
             else
-                exec ~/lib/nagios/plugins/check_icmp -${ip_familly} -w "${warn}" -c "${crit}" "$dest"
+                    rt=0 rt_display=0 rt_min=0 rt_max=0
             fi
-        )
-        ret=$?
-        exit $ret
+            state="OK"
+            [ $loss -ge $loss_warn -o $rt -ge $rt_warn ] && state="WARNING"
+            [ $loss -ge $loss_crit -o $rt -ge $rt_crit ] && state="CRITICAL"
+
+            echo "$state - $dest : rta ${rt_display}ms, lost ${loss}% (fastpinger at $fdate)|rta=${rt_display}ms;${rt_warn};${rt_cirt};0; pl=${loss}%;${loss_warn};${loss_crit};; rtmax=${rt_max}ms;;;; rtmin=${rt_min}ms;;;;"
+            case $state in
+                OK) RET=0;;
+                WARNING) RET=1;;
+                CRITICAL) RET=2;;
+            esac
+        else
+            exec ~/lib/nagios/plugins/check_icmp -${ip_familly} -w "${warn}" -c "${crit}" "$dest"
+        fi
     fi
 else
     echo "fastchecker hook.sh called with invalid mode: $mode"
-    exit 3
 fi
-exit 3
+[ ! "$RET" ] && RET=3
+exit $RET
